fdfeaadbe92340e371e7d5e9111d2186010bce08
Colin <cstatro@gmail.com>
2019-09-18 23:58:09 -0400

    styled out basic layout for front-end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Navbar/NavBar";
 
@@ -7,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="main-area"></div>
     </div>
   );
 }
--- src/Navbar/NavBar.jsx
+++ src/Navbar/NavBar.jsx
@@ -3,7 +3,14 @@ import React, { Component } from "react";
 class NavBar extends Component {
   state = {};
   render() {
-    return <div className="nav"></div>;
+    return (
+      <div className="nav">
+        <h1 className="logo">Pod People</h1>
+        <h2 className="nav-link">Home</h2>
+        <h2 className="nav-link">Browse</h2>
+        <h2 className="nav-link">Lists</h2>
+      </div>
+    );
   }
 }
 
