5ba26947e80774acf39c242d3bebed011aaef96d
Colin <cstatro@gmail.com>
2019-09-19 14:55:12 -0400

    search bar component is displaying

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
-const SearchBar = (props) => {
+import React from "react";
+const SearchBar = props => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("you did it");
+  };
+  return (
+    <div onSubmit={handleSubmit} className="searchbar">
+      <form action="">
+        <input
+          type="text"
+          className="search-text"
+          placeholder="Search For Podcast"
+        />
+        <input type="submit" value="" className="search-button" />
+      </form>
+    </div>
+  );
+};
 
-    return ( <div className="searchbar"></div> );
-}
- 
-export default ;
+export default SearchBar;
--- src/components/SearchColumn.jsx
+++ src/components/SearchColumn.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 const SearchColumn = props => {
-  return <div className="search-column"></div>;
+  return (
+    <div className="search-column">
+      <SearchBar />
+    </div>
+  );
 };
 
 export default SearchColumn;
