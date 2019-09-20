7ec4b1ba51d485a1366ee7de5bc59d18d10ae47e
Colin <cstatro@gmail.com>
2019-09-20 09:58:05 -0400

    added new card component to display podcast info

---
+++ src/components/ShowDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShowDetails = props => {
+  return <div className="podcast-show-card"></div>;
+};
+
+export default ShowDetails;
--- src/containers/PodCastShow.jsx
+++ src/containers/PodCastShow.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+const Parser = require("rss-parser");
 
 const PodCastShow = props => {
-  useEffect;
+  const [rssFeed, setRssFeed] = useState({});
+  useEffect(() => {
+    fetch("http://localhost:3000/grab-feed", {
+      headers: {
+        feed: props.rss
+      }
+    })
+      .then(r => r.text())
+      .then(text => {
+        const parser = new Parser();
+        parser.parseString(text, (err, rss) => {
+          console.log(rss);
+        });
+      });
+  }, []);
   return <div className="podcast-show page">{"Show page time!"}</div>;
 };
 
