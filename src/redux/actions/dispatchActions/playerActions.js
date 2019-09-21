be1dff9b71608524eadb1778b9874f38530e59ae
Colin <cstatro@gmail.com>
2019-09-21 17:56:21 -0400

    dispatch action created for passing track info

---
+++ src/redux/actions/dispatchActions/playerActions.js
@@ -0,0 +1,7 @@
+// const mapDispatchActions = dispatch => ({
+//     setHistory: str => dispatch({ type: "ADD HISTORY", text: str })
+//   });
+
+export const mapPlayerDispatchAction = dispatch => ({
+  setCurrentTrack: link => dispatch({ type: "SET_CURRENT_TRACK", link })
+});
