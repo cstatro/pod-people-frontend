b3f2077a90fdc17ebcfc2c9b190cad6f07519d67
Colin <cstatro@gmail.com>
2019-09-22 20:51:37 -0400

    committing some form values

--- src/components/ListManager.jsx
+++ src/components/ListManager.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useState } from "react";
+import NewListForm from "./NewListForm";
 const ListManager = props => {
   const [viewForm, setViewForm] = useState(false);
   return (
     <div className="list-manager">
       {viewForm ? (
-        <h2>Hi</h2>
+        <NewListForm />
       ) : (
         <button onClick={() => setViewForm(true)}>New List</button>
       )}
---
+++ src/components/NewListForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NewListForm = props => {
+  return (
+    <form action="" className="new-list">
+      <input type="text" />
+      <input type="text" />
+      <input type="submit" value="Create New List" />
+    </form>
+  );
+};
+
+export default NewListForm;
