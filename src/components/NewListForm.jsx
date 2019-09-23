375ca2fd9d039a5dfa615400cb57b748f7266c23
Colin <cstatro@gmail.com>
2019-09-22 22:04:02 -0400

    added handle change logic

--- src/components/NewListForm.jsx
+++ src/components/NewListForm.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 
 class NewListForm extends Component {
+  state = { name: "", description: "" };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
     return (
       <form className="new-list">
-        <input type="text" />
-        <input type="text" />
+        <input
+          onChange={this.handleChange}
+          name="name"
+          placeholder="Name"
+          type="text"
+        />
+        <input
+          onChange={this.handleChange}
+          name="description"
+          placeholder="Description"
+          type="text"
+        />
         <input type="submit" value="Create New List" />
       </form>
     );
