a2e367a0b4c75f55234009a5a74c080b3969d1ec
Colin <cstatro@gmail.com>
2019-09-24 13:20:47 -0400

    ok hot list is now displaying

--- src/components/HotLists.jsx
+++ src/components/HotLists.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { HotListState } from "../redux/actions/mapStateToProps/HotListState";
 
 const HotLists = props => {
+  const { user } = props;
+  const [hotLists, setHotLists] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/lists")
+      .then(r => r.json())
+      .then(lists => setHotLists(lists));
+  }, []);
+
   return (
     <div className="hot-lists">
       <h1>Hot Lists</h1>
+      <ul>
+        {hotLists
+          .filter(l => l.user_id != user.id)
+          .map(l => (
+            <li>{l.name}</li>
+          ))}
+      </ul>
     </div>
   );
 };
