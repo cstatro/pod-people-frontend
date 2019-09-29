9c930d89387366a2770a1079604ff0b2b4b16459
Colin <cstatro@gmail.com>
2019-09-29 17:55:24 -0400

    ok got the list browsing section working

--- src/components/ListSearch.jsx
+++ src/components/ListSearch.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import CurrentUserListRow from "./CurrentUserListRow";
 
 const ListSearch = props => {
-  return <div className="list-search-box"></div>;
+  const [listArr, setLists] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/lists")
+      .then(r => r.json())
+      .then(json => setLists(json));
+  }, []);
+  return (
+    <div className="list-search-box">
+      <input name="search" type="text" />
+      <div className="list-results">
+        {listArr.map(l => (
+          <CurrentUserListRow list={l} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ListSearch;
