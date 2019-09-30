78052d36a8e6b14016c9d15bc61feba7720d39c2
Colin <cstatro@gmail.com>
2019-09-30 19:02:43 -0400

    added api logic in with multiple hooks to handle display

--- src/components/ListSearch.jsx
+++ src/components/ListSearch.jsx
@@ -2,20 +2,46 @@ import React from "react";
 import { useEffect, useState } from "react";
 import CurrentUserListRow from "./CurrentUserListRow";
 
-const ListSearch = props => {
+const ListSearch = () => {
   const [listArr, setLists] = useState([]);
+  const [filtered, setFiltered] = useState(false);
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/lists")
       .then(r => r.json())
       .then(json => setLists(json));
   }, []);
+  useEffect(() => {
+    if (filtered) {
+      fetch("http://localhost:3000/search", {
+        headers: {
+          search
+        }
+      })
+        .then(r => r.json())
+        .then(json => setResult(json));
+    }
+  }, [filtered]);
+
+  const handleChange = e => {
+    if (e.target.value === "") {
+      setFiltered(false);
+    } else {
+      setSearch(e.target.value.split(" "));
+    }
+  };
+
   return (
     <div className="list-search-box">
-      <input name="search" type="text" />
+      <div className="list-search-top">
+        <input onChange={handleChange} name="search" type="text" />
+        <button>search</button>
+      </div>
       <div className="list-results">
-        {listArr.map(l => (
-          <CurrentUserListRow list={l} />
-        ))}
+        {filtered
+          ? result.map(l => <CurrentUserListRow list={l} />)
+          : listArr.map(l => <CurrentUserListRow list={l} />)}
       </div>
     </div>
   );
