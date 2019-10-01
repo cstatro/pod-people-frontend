f64ca3962350a772e6de2850e09e8b7e8bc9b2bc
Colin <cstatro@gmail.com>
2019-10-01 14:10:10 -0400

    profile info showing up

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,13 +5,15 @@ import { mostFollowers, sortByFollowerTotal } from "../helpers/sortHelpers";
 
 class Home extends Component {
   render() {
-    const { name, lists } = this.props.user;
+    const { name, lists, profile_pic, bio } = this.props.user;
     // mostFollowers("here", lists);
     // sortByFollowerTotal(lists)[lists.length - 1];
     return (
       <div className="home page">
         <div className="user-profile">
           <h2>{name}</h2>
+          <img src={profile_pic} alt="" />
+          <p>{bio}</p>
           <h2>Number of Lists: {lists.length}</h2>
           <h2>
             Most Popular List:{" "}
